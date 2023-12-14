563edecebe225040e528fb88738b07e9052b0075
David Shahbazyan Mushegh <david.shahbazyan.m@totogaming.am>
2023-12-14 11:58:29 +0400

    The banner slider now all functionality with api

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ const pageContent = document.querySelector("[page-content]");
 siderbar();
 
 const genreList = {
-   asString(genreIdList) {
+    asString(genreIdList) {
         let newGenreList = [];
 
         for (const genreId of genreIdList) {
@@ -16,32 +16,27 @@ const genreList = {
         };
 
         return newGenreList.join(", ")
-   }
+    }
 };
 
-    fetchData(`https://api.themoviedb.org/3/genre/movie/list?api_key=${api_key}`, function ({ genres }) {
-        for (const { id, name } of genres) {
-            genreList[id] = name;
-        };
+fetchData(`https://api.themoviedb.org/3/genre/movie/list?api_key=${api_key}`, function ({ genres }) {
+    for (const { id, name } of genres) {
+        genreList[id] = name;
+    };
 
-        fetchData(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&page=1`, heroBanner);
-    });
-
-
+    fetchData(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}&page=1`, heroBanner);
+});
 
 function heroBanner({ results: movieList }) {
     const banner = document.createElement("section");
     banner.classList.add("banner");
-    banner.ariaLabel = "Popular Movies";
+    banner.setAttribute("aria-label", "Popular Movies");
 
     banner.innerHTML = `
-    <div class="banner-slider"> </div>
-
-
-
-<div class="slider-control">
-    <div class="control-inner"></div>
-</div>
+        <div class="banner-slider"> </div>
+        <div class="slider-control">
+            <div class="control-inner"></div>
+        </div>
     `;
 
     let controlIndex = 0;
@@ -62,24 +57,20 @@ function heroBanner({ results: movieList }) {
         sliderItem.setAttribute("slider-item", "");
 
         sliderItem.innerHTML = `
-        <img src="${imgBaseURL}w1280${backdrop_path}" alt="${title}" class="img-cover" loading="${index === 0 ? "eager" : "lazy"}">
-					<div class="banner-content">
-						<h2 class="heading">${title}</h2>
-                        <div class="meta-list">
-						<div class="meta-item">${release_date.split("-")[0]}</div>
-						<div class="meta-item card-badge">${vote_average.toFixed(1)}</div>
-                        </div>
-
-						<p class="genre">${genreList.asString(genre_ids)}</p>
-						<p class="banner-text">${overview}</p>
-
-						<a href="./detail.html" class="btn">
-							<img src="./assets/images/play_circle.png" alt="circle" width="24" height="24"
-								arial-hidden="true">
-							<span class="span">Wtach Now</span>
-						</a>
-
-					</div>
+            <img src="${imgBaseURL}w1280${backdrop_path}" alt="${title}" class="img-cover" loading="${index === 0 ? "eager" : "lazy"}">
+            <div class="banner-content">
+                <h2 class="heading">${title}</h2>
+                <div class="meta-list">
+                    <div class="meta-item">${release_date.split("-")[0]}</div>
+                    <div class="meta-item card-badge">${vote_average.toFixed(1)}</div>
+                </div>
+                <p class="genre">${genreList.asString(genre_ids)}</p>
+                <p class="banner-text">${overview}</p>
+                <a href="./detail.html" class="btn">
+                    <img src="./assets/images/play_circle.png" alt="circle" width="24" height="24" aria-hidden="true">
+                    <span class="span">Watch Now</span>
+                </a>
+            </div>
         `;
 
         banner.querySelector(".banner-slider").append(sliderItem);
@@ -89,13 +80,38 @@ function heroBanner({ results: movieList }) {
         controlItem.setAttribute("slider-control", `${controlIndex}`);
         controlIndex++;
         controlItem.innerHTML = `
-        <img src="${imgBaseURL}w154${poster_path}" alt="slide to ${title}" loading="lazy" draggable="false"
-        class="img-cover">
+            <img src="${imgBaseURL}w154${poster_path}" alt="slide to ${title}" loading="lazy" draggable="false" class="img-cover">
         `;
         banner.querySelector(".control-inner").append(controlItem);
-    };
+    }
 
     pageContent.append(banner);
 
-    // addHeroSlide(banner);
-};
+    addHeroSlide();
+}
+
+function addHeroSlide() {
+    const sliderItem = document.querySelectorAll("[slider-item]");
+    const sliderControl = document.querySelectorAll("[slider-control]");
+
+    let lastSlide = sliderItem[0];
+    let lastControl = sliderControl[0];
+
+    lastSlide.classList.add("active");
+    lastControl.classList.add("active");
+
+    function sliderStart() {
+        lastSlide.classList.remove("active");
+        lastControl.classList.remove("active");
+
+        lastSlide = sliderItem[Number(this.getAttribute("slider-control"))];
+        lastControl = this;
+
+        lastSlide.classList.add("active");
+        lastControl.classList.add("active");
+    }
+
+    sliderControl.forEach(control => {
+        control.addEventListener("click", sliderStart);
+    });
+}
